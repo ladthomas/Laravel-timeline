aeebf881f2b7805586eeb96759330b745411d315
ladthomas <ladouyouthomas@outlook.fr>
2024-06-17 17:08:47 +0200

    modified index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
                     @csrf
                     <div class="mb-4">
                         <label for="body" class="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2">Post</label>
-                        <textarea name="body" id="body" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-200 leading-tight focus:outline-none focus:shadow-outline" maxlength="180" required></textarea>
+                        <textarea name="body" id="body" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline" maxlength="180" required></textarea>
                     </div>
                     <div class="flex items-center justify-between">
                         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
